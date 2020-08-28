2980a9055494a9ba20b1f70e29f8650a406334c5
Camila Monreal <moncml@outlook.com>
2020-08-28 13:14:40 -0300

    deploy

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+require('dotenv').config()
+
 require('./database');
 
 app.use(express.json());
 app.use(routes);
-app.listen(3000,() => console.log(`API running on port 3000 ...`));
+app.listen(process.env.PORT || 3000,() => console.log(`API running on port 3000 ...`));
